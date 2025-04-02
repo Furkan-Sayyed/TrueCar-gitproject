cdb6d599dfec3a51af2b5c5c5494344ee3a30c19
Furkan Sayyed <furkansayyed@apnacollege.in>
2025-04-02 14:01:13 +0530

    my first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Furkan Sayyed\OneDrive\Desktop\TrueCar-project\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
